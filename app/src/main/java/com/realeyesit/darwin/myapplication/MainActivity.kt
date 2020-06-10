652e7b845f14170b81dfe748a52510afb08b0aed
Balazs Banyai <balazs.s.banyai@gmail.com>
2020-06-10 14:48:46 +0200

    Update MainActivity.kt

--- app/src/main/java/com/realeyesit/darwin/myapplication/MainActivity.kt
+++ app/src/main/java/com/realeyesit/darwin/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // hello
+        // hellox
         setContentView(R.layout.activity_main)
     }
 }
